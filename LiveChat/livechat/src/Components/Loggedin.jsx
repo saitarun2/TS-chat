41853c54679fda6e8d63b96b9e97f0763915a7d5
Tarun <tarun.nagasai.92@gmail.com>
2024-11-09 13:49:32 +0530

    small change after login

---
+++ LiveChat/livechat/src/Components/Loggedin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Loggedin() {
+  return (
+    <div>Loggedin</div>
+  )
+}
+
+export default Loggedin
--- LiveChat/livechat/src/Components/Login.js
+++ LiveChat/livechat/src/Components/Login.js
@@ -1,16 +1,21 @@
 import React, { useState } from 'react'
+import Loggedin from './Loggedin';
 
 function Login() {
   let [userName,setUserName]=useState("");  
   let [password,setPassword]=useState("");
   let [pop,setPop]=useState("");
-  let [checkClicked,setCheckClicked]=useState(false);
+  let [checkClicked, setCheckClicked] = useState(false);
+  let [logged, setlogged] = useState(false);
   let checkLoginDetails=(e)=>{
     // {setPop(userName+password)}
 
     if(password.length>3){
 
-        alert("hi "+userName+"");
+      alert("hi " + userName + "");
+      console.log('logged in');
+      setlogged(true)
+
         // setCheckClicked(true);
 
 
@@ -26,23 +31,26 @@ function Login() {
   return (
     <>
     
-
-    <div>
-    {!checkClicked ? 
-        <form onSubmit={e=> {
-            e.preventDefault();
-            checkLoginDetails();} }>
+      {!logged ?
+        <div>
+          {!checkClicked ?
+            <form onSubmit={e => {
+              e.preventDefault();
+              checkLoginDetails();
+            }}>
 
         
             
-            <input type='text' placeholder='username' onChange={e => {setUserName(e.target.value)}}/>
-            <input type='password' placeholder='password' onChange={e => {setPassword(e.target.value)}}/>
-           {/* <h1> {userName}</h1>  */}
-            <input type='submit'></input>
+              <input type='text' placeholder='username' onChange={e => { setUserName(e.target.value) }} />
+              <input type='password' placeholder='password' onChange={e => { setPassword(e.target.value) }} />
+              {/* <h1> {userName}</h1>  */}
+              <input type='submit'></input>
 
-        </form>
- :<h1>Blocked from this website.</h1>}
-    </div>
+            </form>
+            : <h1>Blocked from this website.</h1>}
+        </div>
+        :
+        <h1>Logged In</h1>}
             
 
 
